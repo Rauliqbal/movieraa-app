246198ab62e560bec059a03f02339d7d3d9afdcf
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2023-06-17 20:13:42 +0700

    first commit

---
+++ lib/globals.dart
@@ -0,0 +1,6 @@
+class Globals {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "6c5ae941b98ff4368470dfbe28aa0758";
+  static const String imgUrlW500 = "https://image.tmdb.org/t/p/original";
+  static const String imgUrlOri = "https://image.tmdb.org/t/p/original";
+}
